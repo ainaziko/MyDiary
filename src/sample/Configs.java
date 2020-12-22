8670e46ddb5be549d903669759213557c8b08b7b
Ainazik <58284183+ainazimo@users.noreply.github.com>
2020-12-22 17:57:21 +0600

    Update Configs.java

--- src/sample/Configs.java
+++ src/sample/Configs.java
@@ -4,7 +4,7 @@ public class Configs {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "Asadjioli0";
+    protected String dbPass = "";
     protected String dbName = "users";
 
 }
